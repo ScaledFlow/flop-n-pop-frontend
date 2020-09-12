65c5e1d7c8d68c792aaae805a1db143b0a9d3bed
John Leintz <johnleintz@scaledflow.com>
2020-09-12 13:51:52 -0500

    updated react-scripts version

--- README.old.md
+++
@@ -1,6 +0,0 @@
-# flop-n-pop-frontend
-
-- npm install -g create-react-app
-- create-react-app flop-n-pop-frontend --scripts-version 1.1.5
-- cd flop-n-pop-frontend/
-- npm start
--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 
 
